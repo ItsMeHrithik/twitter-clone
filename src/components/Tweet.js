de583198c098412847499ab9a679f50e12751dc2
hrithik_bt18 <hrithik_bt18@iiitkalyani.ac.in>
2021-07-10 11:50:51 +0530

    added event handlers

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -2,12 +2,20 @@ import React, { Component } from "react";
 
 export default class Tweet extends Component {
   render() {
+
+ const deleteTweet = () => {
+     alert(`Tweet will be deleted`);
+}
+
+const likeTweet = () => {
+    alert(`Tweet is Liked`);
+}
     return (
       <div>
         <h2>UserName : {this.props.name}</h2>
         <h3>This will be a tweet</h3>
-        <button>Like</button>
-        <button>Delete</button>
+        <button onClick={likeTweet} >Like</button>
+        <button onClick={deleteTweet}>Delete</button>
       </div>
     );
   }
