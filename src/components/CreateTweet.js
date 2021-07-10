ceed38b91d4b6ffce02560401cd7134924c2f4aa
hrithik_bt18 <hrithik_bt18@iiitkalyani.ac.in>
2021-07-10 14:01:52 +0530

    Learned how to use UseState

--- src/components/CreateTweet.js
+++ src/components/CreateTweet.js
@@ -1,13 +1,30 @@
-import React, { Component } from 'react'
+import React, { useState } from "react";
 
-export default class CreateTweet extends Component {
-    render() {
-        return (
-            <form>
-                <h2>Tweet your thoughts</h2>
-                <textarea cols="90" rows="2"></textarea>
-                <button>Submit</button>
-            </form>
-        )
-    }
-}
+const CreateTweet = () => {
+  const [textInput, setTextInput] = useState("");
+  const [tweets, setTweets] = useState([]);
+
+  const userInputHandler = (e) => {
+    setTextInput(e.target.value);
+  };
+
+  const submitTweetHandler = (e) => {
+    e.preventDefault();
+    setTweets([...tweets, textInput]);
+    setTextInput('');
+  };
+
+  return (
+    <form onSubmit={submitTweetHandler}>
+      <h2>Tweet your thoughts</h2>
+      <textarea
+        value={textInput}
+        onChange={userInputHandler}
+        cols="90"
+        rows="2"
+      ></textarea>
+      <button>Submit</button>
+    </form>
+  );
+};
+export default CreateTweet;
